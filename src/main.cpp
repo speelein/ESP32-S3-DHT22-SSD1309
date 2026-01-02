f4bb6cc58e4bc63768cd527f81afb68503821ea4
Guenter Pruefer <pruefer.g@gmx.de>
2026-01-02 07:40:19 +0100

    124 - Wire.setClock(100000);

--- src/main.cpp
+++ src/main.cpp
@@ -118,10 +118,10 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* 
 void setup() {
   Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
-
+  // Wire.begin(); // funktioniert nicht
   dht.begin();
   u8g2.begin();
-
+  Wire.setClock(100000); // setzt SCL auf 100kHz oder 400 kHz
 }
 
 
